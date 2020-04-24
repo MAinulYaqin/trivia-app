d65bdde5ab3a9f2d5d5942e6b499ae9d6654fcc6
MAinulYaqin <ainul.y9b@gmail.com>
2020-04-24 19:03:53 +0700

    final

--- app/src/main/java/com/gabutproject/triviaapp/GameFragment.kt
+++ app/src/main/java/com/gabutproject/triviaapp/GameFragment.kt
@@ -1,10 +1,10 @@
 package com.gabutproject.triviaapp
 
 import android.os.Bundle
-import android.text.style.QuoteSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -25,7 +25,46 @@ class GameFragment : Fragment() {
         Question(
             text = "Build system for Android?",
             answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")
+        ),
+        Question(
+            text = "Pushing structured data into a Layout?",
+            answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")
+        ),
+        Question(
+            text = "Inflate layout in fragments?",
+            answers = listOf(
+                "onCreateView",
+                "onActivityCreated",
+                "onCreateLayout",
+                "onInflateLayout"
+            )
+        ),
+        Question(
+            text = "Build system for Android?",
+            answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")
+        ),
+        Question(
+            text = "Android vector format?",
+            answers = listOf(
+                "VectorDrawable",
+                "AndroidVectorDrawable",
+                "DrawableVector",
+                "AndroidVector"
+            )
+        ),
+        Question(
+            text = "Android Navigation Component?",
+            answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")
+        ),
+        Question(
+            text = "Registers app with launcher?",
+            answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")
+        ),
+        Question(
+            text = "Mark a layout for Data Binding?",
+            answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>")
         )
+
     )
 
     lateinit var currentQuestion: Question
@@ -70,19 +109,10 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
-                        // view.findNavController().navigate(
-                        //                            GameFragmentDirections.actionGameFragmentToGameWonFragment(
-                        //                                numQuestions,
-                        //                                questionIndex
-                        //                            )
-                        //                        )
                         view.findNavController()
                             .navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
-                    // Game over! A wrong answer sends us to the gameOverFragment.
-                    // view.findNavController()
-                    //    .navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment())
                     view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
                 }
             }
@@ -104,5 +134,7 @@ class GameFragment : Fragment() {
         answers = currentQuestion.answers.toMutableList()
         // and shuffle em
         answers.shuffle()
+        (activity as AppCompatActivity).supportActionBar?.title =
+            getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
     }
 }
--- app/src/main/java/com/gabutproject/triviaapp/TitleFragment.kt
+++ app/src/main/java/com/gabutproject/triviaapp/TitleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.gabutproject.triviaapp.databinding.TitleFragmentBinding
@@ -24,6 +25,9 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+        // set the header title back to normal
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.app_name)
+
         return binding.root
     }
 
